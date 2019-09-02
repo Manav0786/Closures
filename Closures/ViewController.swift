a8d1e425666f66628d1580331db24a1b91b9432c
Manav0786 <manavmahajan93@gmail.com>
2019-09-02 10:45:53 +0530

    Initial Commit

---
+++ Closures/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Closures
+//
+//  Created by Manav Mahajan on 02/09/19.
+//  Copyright © 2019 Manav Mahajan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
